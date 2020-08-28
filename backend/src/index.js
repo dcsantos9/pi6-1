9a5c0fdd74164fb732da2a7496ad8d190b7af040
Fernando Henrique <liquuid@gmail.com>
2020-08-27 21:30:13 -0300

    enable nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/projects', (request, response) => {
-    return response.json({ message: 'Hello'});
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World'});
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('backend started! 🍺');
+});
