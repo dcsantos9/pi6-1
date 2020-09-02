a3d478d4ef1bc7e9ab9419a6a440af2b4f9d6ccd
Fernando Henrique <liquuid@gmail.com>
2020-09-02 02:03:23 -0300

    adding some routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,41 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
+app.get('/pets', (request, response) => {
+    return response.json([ 
+        'lulu',
+        'xuxa',
+        'billu',
+    ]);
 });
+
+app.post('/pets', (request, response) => {
+    return response.json([ 
+        'lulu',
+        'xuxa',
+        'billu',
+        'zika',
+    ]);
+});
+
+app.put('/pets/:id', (request, response) => {
+    return response.json([ 
+        'toto',
+        'xuxa',
+        'billu',
+        'zika',
+    ]);
+});
+
+app.delete('/pets/:id', (request, response) => {
+    return response.json([ 
+        'toto',
+        'xuxa',
+        'zika',
+    ]);
+});
+
+
 
 app.listen(3333, () => {
     console.log('backend started! 🍺');
