c43fcabe69b8df19677f4a6b8d29a01f1bf4a822
Fernando Henrique <liquuid@gmail.com>
2020-09-16 23:48:58 -0300

    initial, can create user and manage avatar

---
+++ backend/src/routes/users.routes.ts
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateAvatarUserService from '../services/UpdateUserAvatarService';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+upload.array
+
+usersRouter.post('/', async (request, response) => { 
+
+        const { name, email, password } = request.body;
+
+        const createUser = new CreateUserService();
+
+        const user = await createUser.execute({
+            name,
+            email,
+            password,
+        });
+        delete user.password;
+        
+        return response.json(user);
+
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async ( request, response ) => {
+
+        const updateUserAvatar = new UpdateAvatarUserService();
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename,
+        });
+        delete user.password
+        return response.json(user);
+
+    
+} );
+
+ export default usersRouter;
