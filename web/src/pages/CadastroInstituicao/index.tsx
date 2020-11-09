58917d1f007173201f9e0a480e96f405fc0de741
Fernando Henrique <liquuid@gmail.com>
2020-11-09 13:55:44 -0300

    form user get saved data from localstorage

--- web/src/pages/CadastroInstituicao/index.tsx
+++ web/src/pages/CadastroInstituicao/index.tsx
@@ -4,7 +4,7 @@ import { Link, useHistory } from 'react-router-dom';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import { Container, Content, AnimationContainer, Background, Image } from './styles';
-import { useAuth } from '../../hooks/auth';
+import { useAuth, AuthProvider } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
 //import InputFormulario from '../../components/InputFormulario';
 import Input from '../../components/Input';
@@ -14,10 +14,12 @@ import getValidationErrors from '../../utils/getValidationErrors';
 import imgPhoto from '../../assets/cat-login.jpg';
 import api from '../../services/api';
 import { FiUser , FiMail , FiLock, FiPhone, FiMapPin } from 'react-icons/fi';
-import { FaRegAddressCard } from "react-icons/fa";
+import { FaLessThan, FaRegAddressCard } from "react-icons/fa";
 import { AiOutlineFieldNumber } from "react-icons/ai";
 import  MainMenu  from '../../components/MainMenu';
 import Select from '../../components/Select';
+import userEvent from '@testing-library/user-event';
+import { request } from 'http';
 
 interface CadastroInstituicaoFormData {
     name: string;
@@ -41,6 +43,7 @@ const CadastroInstituicao: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
     const {addToast} = useToast();
     const history = useHistory();
+    const user = JSON.parse(localStorage.getItem('@QueroPet:user') || "{}");
 
     const handleSubmit = useCallback( async (data: object) => {
         try {
@@ -85,6 +88,14 @@ const CadastroInstituicao: React.FC = () => {
         }
 
     },[addToast, history]);
+
+    const cnpj_radio_selected = user.social_id_type === 'cnpj' ? true : false;
+    const cpf_radio_selected = user.social_id_type === 'cpf' ? true : false;
+
+    const phone_type_work = user.phone_type === 'work' ? true : false;
+    const phone_type_mobile = user.phone_type === 'mobile' ? true : false;
+    const phone_type_home = user.phone_type === 'home' ? true : false;
+
     return (
         <Container>
             <MainMenu>
@@ -111,42 +122,44 @@ const CadastroInstituicao: React.FC = () => {
                         <Image src={'https://source.unsplash.com/user/erondu/600x400'}></Image>
                             <input type="file" id="file" name="filename" value="" />
                             <Button type="submit" name="sendPhoto" className="button button2">enviar</Button>
-                            <Input name="name" placeholder="Nome" icon={FiUser} />
+                            <Input name="name" value={user.name} placeholder="Nome" icon={FiUser}/>
 
                             <h3><span>Dados</span></h3>
                             <label>
-                                <input type="radio" value="CNPJ" className="radio" /> CNPJ
+                                <input type="radio" value="CNPJ" className="radio" checked={cnpj_radio_selected}/> CNPJ
                             </label>
                             <label>
-                                <input type="radio" value="CPF" className="radio" /> CPF
+                                <input type="radio" value="CPF" className="radio" checked={cpf_radio_selected}/> CPF
                             </label>
 
-                            <Input name="social_id" placeholder="00.000.000/0000-00" icon={FaRegAddressCard}/>
-                            <textarea name="info" placeholder="informações" />
+                            <Input name="social_id" value={user.social_id} placeholder="00.000.000/0000-00" icon={FaRegAddressCard}/>
+                            <textarea name="info" value={user.info} placeholder="informações" />
                         <h3><span>Contato</span></h3>
-                            <Input name="email" placeholder="email@email.com.br" icon={FiMail} />
-                            <Input name="phone" placeholder="(XX) XXXXX-XXXX" icon={FiPhone} />
+                            <Input name="email" value={user.email} placeholder="email@email.com.br" icon={FiMail} />
+                            <Input name="phone" value={user.phone} placeholder="(XX) XXXXX-XXXX" icon={FiPhone} />
                             <select>
-                                <option key="MOBILE" value="MOBILE" >celular</option>
-                                <option key="HOME" value="HOME">residencial</option>
-                                <option key="WORK" value="WORK" >trabalho</option>
+                                <option key="MOBILE" value="MOBILE" selected={phone_type_mobile}>celular</option>
+                                <option key="HOME" value="HOME" selected={phone_type_home}>residencial</option>
+                                <option key="WORK" value="WORK" selected={phone_type_work}>trabalho</option>
                             </select>
                         <a href="">adicionar outro telefone</a>
                         <h3><span>Endereço</span></h3>
-                            <Input name="street" placeholder="rua, avenida" icon={FiMapPin} />
-                            <Input name="number" placeholder="número" icon={AiOutlineFieldNumber} />
-                            <Input name="complement" placeholder="complemento, bloco, apartamento, casa" icon={FiMapPin} />
-                            <Input name="neightborhood" placeholder="bairro" icon={FiMapPin} />
-                            <Input name="city" placeholder="cidade" icon={FiMapPin} />
-                            <Input name="state" placeholder="uf" icon={FiMapPin}  />
+                            <Input name="street" value={user.street} placeholder="rua, avenida" icon={FiMapPin} />
+                            <Input name="number" value={user.number} placeholder="número" icon={AiOutlineFieldNumber} />
+                            <Input name="complement"value={user.complement}  placeholder="complemento, bloco, apartamento, casa" icon={FiMapPin} />
+                            <Input name="neightborhood" value={user.neightborhood} placeholder="bairro" icon={FiMapPin} />
+                            <Input name="city" value={user.city} placeholder="cidade" icon={FiMapPin} />
+                            <Input name="state" value={user.state} placeholder="uf" icon={FiMapPin}  />
                             <label></label>
-                            <Input name="zipcode" placeholder="cep"icon={FiMapPin}  />
+                            <Input name="zipcode" value={user.zipcode}  placeholder="cep"icon={FiMapPin}  />
                         <h3><span>Senha</span></h3>
                             <Input type="password" name="password" placeholder="senha" icon={FiLock} />
                             <Input type="password" name="password" placeholder="confirmar senha" icon={FiLock} />
                             <Button type="submit" className="button">salvar</Button>
                             <div className="button" style={{ float: "right" }}>
                                 <Link to="/">voltar</Link>
+
+
                             </div>
                     </Form>
                 </AnimationContainer>
